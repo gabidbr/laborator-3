1f0ece37fc8db1a99a8c55a176e5eeab3cea73a6
gabidbr <62308761+gabidbr@users.noreply.github.com>
2020-03-17 22:39:46 +0200

    Add files via upload

---
+++ Lab3/src/com/company/Clasa.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Clasa {
+    private int numar;
+    private String nume;
+
+    public Clasa(int numar, String nume) {
+        this.numar = numar;
+        this.nume = nume;
+    }
+
+    public int getNumar() {
+        return numar;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+}
